1afd3d6a1d7bd1c047fe21eb35aa273f9fe1ddab
Somkiet <somsor@perfenterprise.com>
2018-11-03 16:18:46 +0700

    init web and api

---
+++ Hack.Change.Api/Controllers/ChangeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hack.Change.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hack.Change.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ChangeController : Controller
+    {
+        private readonly IChangeCalculator changeCalculator;
+
+        public ChangeController(IChangeCalculator changeCalculator)
+        {
+            this.changeCalculator = changeCalculator;
+        }
+
+        //[HttpGet("{}/{}")]
+        //public IEnumerable<ChangeModel> Calculate()
+        //{
+        //   var result = changeCalculator.CalculateChange()
+        //}
+    }
+}
